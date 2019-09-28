6ab061a905cf7813508ab4b7ac5ebc68de283313
Klein <mklein@dxc.com>
2019-09-28 23:17:32 +0200

    added basic gamepad ui layout

--- src/components/GamepadLayout.vue
+++ src/components/GamepadLayout.vue
@@ -1,29 +1,45 @@
 <template lang="pug">
 div.fullsize
-  div#layout.fullsize
-    GamepadLayout
+  div.left
+  div.center
+  div.right
 
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { GamepadOptions } from '../GamepadOptions';
 import { ControllerlyClient } from 'controllerly-core';
-import GamepadLayout from './GamepadLayout.vue';
 
 /**
  * User enters the connection code that the server provides.
  */
-@Component({
-    components: {
-        GamepadLayout
-    }
-})
-export default class Gamepad extends Vue {
+@Component
+export default class GamepadLayout extends Vue {
   @Prop() private client!: ControllerlyClient;
   @Prop() private options!: GamepadOptions;
 }
 
 </script>
 <style scoped>
-
+.left {
+  position: absolute;
+  width: 40%;
+  height: 100%;
+  left: 0;
+  background-color: red;
+}
+.center {
+  position: absolute;
+  width: 20%;
+  height: 100%;
+  left: 40%;
+  background-color: green;
+}
+.right {
+  position: absolute;
+  width: 40%;
+  height: 100%;
+  right: 0;
+  background-color: red;
+}
 </style>
