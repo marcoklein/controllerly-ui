2ee58147b06dc446a75abbd6a4bd21f28516577b
Marco Klein <marco.klein94@gmail.com>
2019-10-05 11:42:40 +0100

    add images

---
+++ docs/precache-manifest.3845989c5749bbb5aa489255d561438e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "01c9a4073c8b1211684ca8c9797172c6",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "f98296a172192d4740bc64419a72f83c",
+    "url": "/mobile-detect.js"
+  },
+  {
+    "revision": "a7c90e141ac25930307394e08d84d54b",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "054e397d875bfd1d945b200b1bd38027",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "75fc5c484be1eba504a7c72b6b62a280",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "295b441dd12f66ac6c53ba5b9eb21e8b",
+    "url": "/img/scrollHorizontal.295b441d.png"
+  },
+  {
+    "revision": "f16c9dba63338becd5c949e627ada3b1",
+    "url": "/img/scrollVertical.f16c9dba.png"
+  },
+  {
+    "revision": "f97de6f5305441fa7474",
+    "url": "/js/chunk-vendors.8512a5c8.js"
+  },
+  {
+    "revision": "b8396343cc783d1fe5a7",
+    "url": "/js/app.3a52462a.js"
+  },
+  {
+    "revision": "8c4fd46dcc5b9c8df2263b59701c80cc",
+    "url": "/img/unlocked.8c4fd46d.png"
+  },
+  {
+    "revision": "043c50e1d76b404e7a12916ba2b2940d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ba1b73c57cc9030a466c22da7a0da04",
+    "url": "/img/locked.9ba1b73c.png"
+  },
+  {
+    "revision": "e1c066468bd2a8c836f4e5206d1a05a7",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "b8396343cc783d1fe5a7",
+    "url": "/css/app.fb380ed8.css"
+  },
+  {
+    "revision": "287419f7ecaf619dce044c60c9df248e",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "a61abd43f244bc9f67e3084b1118b8f2",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "a0b31d6875cefca8031a4ab1edcafd57",
+    "url": "/android-chrome-384x384.png"
+  },
+  {
+    "revision": "927db20b94efffd4a35690238714c3f0",
+    "url": "/android-chrome-192x192.png"
+  }
+];
